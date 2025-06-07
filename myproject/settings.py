44bfa2a366d611b161db87846af4a5b7423e5bc4
Théo KASZAK <theokaszak2@gmail.com>
2025-06-07 15:28:41 +0200

    Final push i guess

--- myproject/settings.py
+++ myproject/settings.py
@@ -143,11 +143,11 @@ if missing_db_vars and ENVIRONMENT == 'production':
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'P-SP-DB01'),
-        'USER': os.getenv('DB_USER', 'theo'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'lolmdr123'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
